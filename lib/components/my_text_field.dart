9ba5e753b98662f7fb5a1137fb517e4a501e6fa8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-02-20 11:43:15 +0200

    create my textfield

---
+++ lib/components/my_text_field.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  const MyTextField(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.obscureText});
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:food/components/my_text_field.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: ListView(
+        children: [
+          Icon(
+            Icons.lock_open_rounded,
+            size: 100,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            'Food Delivery App',
+            style:
+                TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+          ),
+          MyTextField(
+              controller: emailController,
+              hintText: 'Email',
+              obscureText: false),
+        ],
+      ),
+    );
   }
 }

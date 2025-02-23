47a700334a9ed71518012b13e4ed0c5513662cb5
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-02-23 16:21:29 +0200

    finsh my descrption box

--- lib/feature/home/presentation/views/widgets/my_description_box.dart
+++ lib/feature/home/presentation/views/widgets/my_description_box.dart
@@ -2,23 +2,45 @@ import 'package:flutter/material.dart';
 
 class MyDescriptionBox extends StatelessWidget {
   const MyDescriptionBox({super.key});
+
   @override
   Widget build(BuildContext context) {
+    var myPrimaryTextStyel =
+        TextStyle(color: Theme.of(context).colorScheme.inversePrimary);
+    var myscondryTextStyel =
+        TextStyle(color: Theme.of(context).colorScheme.primary);
     return Container(
       padding: EdgeInsets.all(25),
+      margin: EdgeInsets.only(left: 25, right: 25, bottom: 25),
       decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
           border: Border.all(color: Theme.of(context).colorScheme.secondary)),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
             children: [
-              Text('\$0.99'),
-              Text('Delivery fee'),
+              Text(
+                '\$0.99',
+                style: myPrimaryTextStyel,
+              ),
+              Text(
+                'Delivery fee',
+                style: myscondryTextStyel,
+              ),
             ],
           ),
           Column(
-            children: [Text('15-30 min'), Text('Delivery time')],
+            children: [
+              Text(
+                '15-30 min',
+                style: myPrimaryTextStyel,
+              ),
+              Text(
+                'Delivery time',
+                style: myscondryTextStyel,
+              )
+            ],
           ),
         ],
       ),
